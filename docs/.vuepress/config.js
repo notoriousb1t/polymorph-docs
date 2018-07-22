8a9b3583cb6fa466dd5d26144cef922436f2c321
Christopher Wallis <christopher.j.wallis@gmail.com>
2018-07-22 09:08:43 -0400

    travis stuff

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = { 
+  title: "Polymorph",
+  description: "Get Your SVG into Shape!",
+  base: "/polymorph/",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }]
+  ],
+  themeConfig: {
+    nav: [
+        { text: "Setup", link: "/setup.md" },
+        { text: "API Reference", link: "/api.md" }
+    ]
+  }
+}
