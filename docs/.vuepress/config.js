49b765a03f1665b351c491aacc0db5f79dda61fc
Christopher Wallis <christopher.j.wallis@gmail.com>
2024-01-13 18:54:15 -0700

    Upgrade to newest vuepress and vue3 and switch to pnpm

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,18 @@
-module.exports = { 
+import { defaultTheme } from '@vuepress/theme-default'
+
+export default { 
   title: "Polymorph",
+  theme: defaultTheme({
+    navbar: [
+
+      { text: "About", link: "/about.md" },
+      { text: "Setup", link: "/setup.md" },
+      { text: "API Reference", link: "/api.md" }
+    ],
+  }),
   description: "Get Your SVG into Shape!",
   base: "/polymorph-docs/",
   head: [
     ['link', { rel: 'icon', href: '/favicon.png' }]
   ],
-  themeConfig: {
-    nav: [
-        { text: "About", link: "/about.md" },
-        { text: "Setup", link: "/setup.md" },
-        { text: "API Reference", link: "/api.md" }
-    ]
-  }
 }
